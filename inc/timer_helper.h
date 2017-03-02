97563550e5f5acff06850d84261607068a634516
Varun Gupta <varung97@gmail.com>
2017-03-02 12:04:13 +0800

    Add secTicks

--- inc/timer_helper.h
+++ inc/timer_helper.h
@@ -1,3 +1,4 @@
 extern volatile uint32_t msTicks;
+extern volatile uint32_t secTicks;
 
 void setup_timer_interrupt(void);
--- src/timer_helper.c
+++ src/timer_helper.c
@@ -9,14 +9,21 @@
 #include "LPC17xx.h"
 
 volatile uint32_t msTicks; // counter for 1ms SysTicks
+volatile uint32_t secTicks; // counter for 1s SysTicks
 
 //  SysTick_Handler - just increment SysTick counter
 void SysTick_Handler(void) {
 	msTicks++;
+	if (msTicks % 1000 == 0) {
+		secTicks++;
+	}
 }
 
 // Setup SysTick Timer to interrupt at 1 msec intervals
 void setup_timer_interrupt() {
+	msTicks = 0;
+	secTicks = 0;
+
 	if (SysTick_Config(SystemCoreClock / 1000)) {
 		while (1);  // Capture error
 	}
